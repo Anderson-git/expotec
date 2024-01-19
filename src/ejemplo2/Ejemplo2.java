23806943bd76f3f748ce32baf7ccde175f4c2fe1
Anderson Tzunun <progra@gmail.com>
2024-01-19 11:17:31 -0600

    primer respositorio

---
+++ src/ejemplo2/Ejemplo2.java
@@ -0,0 +1,17 @@
+//Anderson Tzunun
+package ejemplo2;
+
+
+public class Ejemplo2 {
+
+  
+    public static void main(String[] args) {
+    //Variable entero tipo byte
+    boolean decision = true;
+   //para asignarle un valor a una variable primero asignar tipo y luego nombre
+        System.out.println("El caracter es: "+decision);
+                
+               
+    }
+    
+}
